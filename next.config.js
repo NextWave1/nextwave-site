4236ddfcaa2c6c93100368fb6514a8ef7e7f883c
Sanchezz <sasha.dadaev.98@mail.ru>
2023-11-06 13:45:16 +0700

    Базовая кнастройка конфигов

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	// reactStrictMode: true,
+	poweredByHeader: false,
+	optimizeFonts: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		SERVER_URL: process.env.REACT_SERVER_URL,
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `${process.env.REACT_SERVER_URL}/api/:path*`,
+			},
+			{
+				source: '/uploads/:path*',
+				destination: `${process.env.REACT_SERVER_URL}/uploads/:path*`,
+			},
+		]
+	},
+}
 
 module.exports = nextConfig
